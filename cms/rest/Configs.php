ddfbfd6449500408532a08803d42eeee53369407
Fabrício da Cunha Santos <fabriciodacunhasantos@gmail.com>
2015-12-02 15:09:17 -0200

    Adicionando arquivo de configuração do banco de dados

---
+++ cms/rest/Configs.php
@@ -0,0 +1,10 @@
+<?php
+//db configuration
+define("dbHostName", "localhost");
+define("dbUsername", "root"); 
+define("dbPassword", "root");
+define("dbName", "gestao");
+
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+define("SECURE", false);
